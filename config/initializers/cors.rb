283c995f568c62bd2d4d3812ae2c3c593af3d11b
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-28 18:36:38 +0430

    Update CORS configuration to allow requests from specific origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Avoid CORS issues when API is called from the frontend app.
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin Ajax requests.
+
+# Read more: https://github.com/cyu/rack-cors
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "*"
- 
-      resource "*",
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      origins 'http://localhost:3001', 'http://172.16.0.32:3001'
+      resource '*', 
+        headers: :any, 
+        methods: [:get]
     end
   end
