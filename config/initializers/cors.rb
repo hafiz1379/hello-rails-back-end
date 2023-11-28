34d56e13b1402fe5e17948d0d309888bd8645910
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-28 20:48:58 +0430

    Update CORS configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,11 +5,12 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:3001', ' http://10.10.10.254:3001'
-      resource '*', 
-        headers: :any, 
-        methods: [:get]
-    end
-  end
+ Rails.application.config.middleware.insert_before 0, Rack::Cors do
+   allow do
+     origins "*"
+
+     resource "*",
+       headers: :any,
+       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+   end
+ end
